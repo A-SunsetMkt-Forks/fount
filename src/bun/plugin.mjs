3417e16735a7285e42a84d19ae9115d6fcc658f7
steve02081504 <steve02081504@EDEN.fukwold>
2025-05-17 17:49:28 +0800

    update

--- src/bun/plugin.mjs
+++ src/bun/plugin.mjs
@@ -1,4 +1,9 @@
 import { plugin } from 'bun'
+import { execSync } from 'node:child_process'
+
+function install(pkgname) {
+	execSync(`bun install ${pkgname}`)
+}
 
 plugin({
 	name: 'deno npm: imports',
@@ -6,13 +11,17 @@ plugin({
 		// npm:xxx -> xxx
 		build.onResolve(
 			{ filter: /.*/, namespace: 'npm' },
-			(args) => ({ path: args.path })
+			(args) => {
+				install(args.path)
+				return {path: args.path }
+			}
 		)
 		// https://deno.land/x/canvas/mod.ts -> canvas
 		build.onResolve(
 			{ filter: /\/\/deno\.land\/x\/(.+?)\//, namespace: 'https' },
 			(args) => {
 				const path = args.path.replace('//deno.land/x/', '').split('/')[0]
+				install(path)
 				return { path }
 			}
 		)
