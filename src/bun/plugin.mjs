2f7846fcdf2ef013506b571229ec9b9d73c16f2c
steve02081504 <steve02081504@EDEN.fukwold>
2025-05-17 13:23:15 +0800

    Update plugin.mjs

--- src/bun/plugin.mjs
+++ src/bun/plugin.mjs
@@ -3,9 +3,18 @@ import { plugin } from 'bun'
 plugin({
 	name: 'deno npm: imports',
 	setup(build) {
+		// npm:xxx -> xxx
 		build.onResolve(
 			{ filter: /.*/, namespace: 'npm' },
 			(args) => ({ path: args.path })
+		)
+		// https://deno.land/x/canvas/mod.ts -> canvas
+		build.onResolve(
+			{ filter: /https:\/\/deno\.land\/x\/(.+?)\// },
+			(args) => {
+				const path = args.path.replace('https://deno.land/x/', '').split('/')[0]
+				return { path }
+			}
 		)
 	},
 })
